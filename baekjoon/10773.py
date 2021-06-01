e13d2e041a9d692249cff6eca336aeb864117ac7
suumnn <suumnq@gmail.com>
2021-06-01 23:00:53 +0900

    BOJ-stack

---
+++ baekjoon/10773.py
@@ -0,0 +1,14 @@
+import sys
+
+k = int(sys.stdin.readline().rstrip())
+stack = []
+
+for _ in range(k):
+    n = int(sys.stdin.readline().rstrip())
+
+    if n == 0:
+        stack.pop()
+    else:
+        stack.append(n)
+
+print(sum(stack))
