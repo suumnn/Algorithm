c961461f79269bc2c83bc4ccd98511cc482867f1
suumnn <suumnq@gmail.com>
2021-07-16 01:37:30 +0900

    learning series

---
+++ baekjoon/learning series/src/Main2558.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main2558 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a + b);
+	}
+}
---
+++ baekjoon/learning series/src/Main3046.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main3046 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int r1 = sc.nextInt();
+		int s = sc.nextInt();
+		sc.close();
+		
+		System.out.println(s * 2 - r1);
+	}
+
+}
