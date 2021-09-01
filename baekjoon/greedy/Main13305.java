89ebf1cc1044d34c78a6c6ba3efcece23b846626
suumnn <suumnq@gmail.com>
2021-09-01 23:44:01 +0900

    greedy

---
+++ baekjoon/greedy/Main13305.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main13305 { // ¡÷¿Øº“
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		long[] dist = new long[n - 1];
+		long[] cost = new long[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n - 1; i++) {
+			dist[i] = Long.parseLong(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			cost[i] = Long.parseLong(st.nextToken());
+		}
+
+		long sum = 0;
+		for (int i = 0; i < n - 1; i++) {
+			if (cost[i] < cost[i + 1]) {
+				cost[i + 1] = cost[i];
+			}
+
+			sum += cost[i] * dist[i];
+		}
+
+		System.out.println(sum);
+	}
+
+}
